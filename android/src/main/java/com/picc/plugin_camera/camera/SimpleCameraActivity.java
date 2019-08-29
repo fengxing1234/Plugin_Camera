c48d8d0f27a1967aed4f6f2303dd6f032c0b250e
fengxing <f296016140@gmail.com>
2019-08-29 15:16:03 +0800

    RQ: 升级androidX

--- android/src/main/java/com/picc/plugin_camera/camera/SimpleCameraActivity.java
+++ android/src/main/java/com/picc/plugin_camera/camera/SimpleCameraActivity.java
@@ -6,14 +6,15 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.FrameLayout;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.picc.plugin_camera.R;
 
